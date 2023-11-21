2a8c6f27f33613481475986b47922b0bfa4002db
jhlin <imjustaup@gmail.com>
2023-11-21 11:37:41 +0800

    build: setup for ImageNotification

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by JH Lin on 2023/11/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
